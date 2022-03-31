d7347fd70f77a47d1d43a5006e7864ddf8598df6
Kamil Szymik <szymik.kamil97@gmail.com>
2022-04-01 00:26:56 +0200

    fix show

--- app/var/cache/dev/twig/f8/f8db30faef7b1318eca924c4ab8c4347cf384e4cbc9c1b6b807cc2668e2668c2.php
+++ app/var/cache/dev/twig/f8/f8db30faef7b1318eca924c4ab8c4347cf384e4cbc9c1b6b807cc2668e2668c2.php
@@ -29,6 +29,7 @@ class __TwigTemplate_4c3429a81403940e7c5f59dc027eec62a8d487e507414cb9fc663d20c82
             'navigation' => [$this, 'block_navigation'],
             'body' => [$this, 'block_body'],
             'stylesheets' => [$this, 'block_stylesheets'],
+            'javascript' => [$this, 'block_javascript'],
         ];
     }
 
@@ -88,21 +89,6 @@ class __TwigTemplate_4c3429a81403940e7c5f59dc027eec62a8d487e507414cb9fc663d20c82
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "navigation"));
 
-        // line 6
-        echo "    <div class=\"navbar\">
-        <div class=\"brandLogoWrapper\">
-            <a class=\"logo\" href=\"";
-        // line 8
-        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("browse");
-        echo "\" target=\"_parent\">
-                <img class=\"logo\" src=\"";
-        // line 9
-        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("images/netflixLogoSmall.svg.png"), "html", null, true);
-        echo "\" alt=\"av1\"/>
-            </a>
-        </div>
-    </div>
-";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
 
@@ -121,57 +107,32 @@ class __TwigTemplate_4c3429a81403940e7c5f59dc027eec62a8d487e507414cb9fc663d20c82
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
 
-        // line 16
-        echo "    <body>
-    <div class=\"movie_container\">
+        // line 17
+        echo "    <div class=\"movie_container\">
         <div class=\"text-center\">
             <div>
-                <iframe width=\"1720\" height=\"800\" src=\"";
+                <iframe id=\"asd\" style=\"border: 0;\" width=\"100%\" height=\"100%\" src=\"";
         // line 20
         echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["movie"]) || array_key_exists("movie", $context) ? $context["movie"] : (function () { throw new RuntimeError('Variable "movie" does not exist.', 20, $this->source); })()), "link", [], "any", false, false, false, 20), "html", null, true);
-        echo "\" frameborder=\"0\"
+        echo "?autoplay=1\"
                         allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\"
                         allowfullscreen>
                 </iframe>
+                <a href=\"";
+        // line 24
+        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("browse");
+        echo "\" target=\"_parent\">
+                <img class=\"logo\" src=\"";
+        // line 25
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("images/netflixLogoSmall.svg.png"), "html", null, true);
+        echo "\" alt=\"av1\"/>
+                </a>
 ";
-        // line 31
+        // line 34
         echo "            </div>
 
-            <div style=\"margin-top: 20px\">
-                ";
-        // line 34
-        if (array_key_exists("liked", $context)) {
-            // line 35
-            echo "                    <form id=\"dislike\" method=\"POST\" action=\"";
-            echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("dislike");
-            echo "\">
-                        <input type=\"text\" name=\"id\" value=\"";
-            // line 36
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["movie"]) || array_key_exists("movie", $context) ? $context["movie"] : (function () { throw new RuntimeError('Variable "movie" does not exist.', 36, $this->source); })()), "id", [], "any", false, false, false, 36), "html", null, true);
-            echo "\" style=\"display: none\">
-                        <a class=\"my-list-button\"
-                           onclick=\"document.getElementById('dislike').submit()\">Usuń z mojej listy</a>
-                    </form>
-                ";
-        } else {
-            // line 41
-            echo "                    <form id=\"like\" method=\"POST\" action=\"";
-            echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("like");
-            echo "\">
-                        <input type=\"text\" name=\"id\" value=\"";
-            // line 42
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["movie"]) || array_key_exists("movie", $context) ? $context["movie"] : (function () { throw new RuntimeError('Variable "movie" does not exist.', 42, $this->source); })()), "id", [], "any", false, false, false, 42), "html", null, true);
-            echo "\" style=\"display: none\">
-                        <a class=\"my-list-button\"
-                           onclick=\"document.getElementById('like').submit()\">Dodaj do mojej listy</a>
-                    </form>
-                ";
-        }
-        // line 47
-        echo "            </div>
         </div>
     </div>
-    </body>
 ";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
@@ -181,7 +142,7 @@ class __TwigTemplate_4c3429a81403940e7c5f59dc027eec62a8d487e507414cb9fc663d20c82
 
     }
 
-    // line 52
+    // line 40
     public function block_stylesheets($context, array $blocks = [])
     {
         $macros = $this->macros;
@@ -191,22 +152,26 @@ class __TwigTemplate_4c3429a81403940e7c5f59dc027eec62a8d487e507414cb9fc663d20c82
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
 
-        // line 53
+        // line 41
         echo "    ";
         $this->displayParentBlock("stylesheets", $context, $blocks);
         echo "
     <style>
 
         ";
-        // line 59
+        // line 47
         echo "
         html {
             font-family: 'Netflix Sans Regular', \"Helvetica Neue\", \"Helvetica\", Arial, sans-serif;
             font-weight: normal;
         }
-
+        iframe {
+            position: relative;
+            /*bottom: 50px;*/
+            /*z-index: -1;*/
+        }
         ";
-        // line 68
+        // line 60
         echo "
         .my-list-button {
             color: gray;
@@ -219,7 +184,7 @@ class __TwigTemplate_4c3429a81403940e7c5f59dc027eec62a8d487e507414cb9fc663d20c82
         }
 
         ";
-        // line 82
+        // line 74
         echo "
         .navbar a:hover {
             opacity: 0.8;
@@ -236,10 +201,38 @@ class __TwigTemplate_4c3429a81403940e7c5f59dc027eec62a8d487e507414cb9fc663d20c82
         }
 
         .logo {
+            position: absolute;
             height: 31px;
-            margin-right: 15px;
+            right: 1760px;
+            top: 30px;
+            z-index: 2;
+        }
+        .movie_container {
+             margin-left: 0px;
         }
     </style>
+
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 102
+    public function block_javascript($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascript"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascript"));
+
+        // line 103
+        echo "
 
 ";
         
@@ -262,7 +255,7 @@ class __TwigTemplate_4c3429a81403940e7c5f59dc027eec62a8d487e507414cb9fc663d20c82
 
     public function getDebugInfo()
     {
-        return array (  223 => 82,  210 => 68,  202 => 59,  195 => 53,  185 => 52,  171 => 47,  163 => 42,  158 => 41,  150 => 36,  145 => 35,  143 => 34,  138 => 31,  131 => 20,  125 => 16,  115 => 15,  100 => 9,  96 => 8,  92 => 6,  82 => 5,  61 => 3,  38 => 1,);
+        return array (  235 => 103,  225 => 102,  188 => 74,  175 => 60,  163 => 47,  156 => 41,  146 => 40,  132 => 34,  127 => 25,  123 => 24,  116 => 20,  111 => 17,  101 => 15,  83 => 5,  62 => 3,  39 => 1,);
     }
 
     public function getSourceContext()
@@ -272,24 +265,27 @@ class __TwigTemplate_4c3429a81403940e7c5f59dc027eec62a8d487e507414cb9fc663d20c82
 {% block title %}<title>{{ movie.title }} - Netflix</title>{% endblock %}
 
 {% block navigation %}
-    <div class=\"navbar\">
-        <div class=\"brandLogoWrapper\">
-            <a class=\"logo\" href=\"{{ path('browse') }}\" target=\"_parent\">
-                <img class=\"logo\" src=\"{{ asset('images/netflixLogoSmall.svg.png') }}\" alt=\"av1\"/>
-            </a>
-        </div>
-    </div>
+{#    <div class=\"navbar\">#}
+{#        <div class=\"brandLogoWrapper\">#}
+{#            <a class=\"logo\" href=\"{{ path('browse') }}\" target=\"_parent\">#}
+{#                <img class=\"logo\" src=\"{{ asset('images/netflixLogoSmall.svg.png') }}\" alt=\"av1\"/>#}
+{#            </a>#}
+{#        </div>#}
+{#    </div>#}
 {% endblock %}
 
 {% block body %}
-    <body>
+{#    <body>#}
     <div class=\"movie_container\">
         <div class=\"text-center\">
             <div>
-                <iframe width=\"1720\" height=\"800\" src=\"{{ movie.link }}\" frameborder=\"0\"
+                <iframe id=\"asd\" style=\"border: 0;\" width=\"100%\" height=\"100%\" src=\"{{ movie.link }}?autoplay=1\"
                         allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\"
                         allowfullscreen>
                 </iframe>
+                <a href=\"{{ path('browse') }}\" target=\"_parent\">
+                <img class=\"logo\" src=\"{{ asset('images/netflixLogoSmall.svg.png') }}\" alt=\"av1\"/>
+                </a>
 {#                <h3>{{ movie.title }}</h3>#}
 {#                <p>Opis : <br>{{ movie.description }}</p>#}
 {#                <p>Rok : {{ movie.year }} <br>#}
@@ -299,24 +295,9 @@ class __TwigTemplate_4c3429a81403940e7c5f59dc027eec62a8d487e507414cb9fc663d20c82
 {#                {% endfor %}#}
             </div>
 
-            <div style=\"margin-top: 20px\">
-                {% if liked is defined %}
-                    <form id=\"dislike\" method=\"POST\" action=\"{{ path('dislike') }}\">
-                        <input type=\"text\" name=\"id\" value=\"{{ movie.id }}\" style=\"display: none\">
-                        <a class=\"my-list-button\"
-                           onclick=\"document.getElementById('dislike').submit()\">Usuń z mojej listy</a>
-                    </form>
-                {% else %}
-                    <form id=\"like\" method=\"POST\" action=\"{{ path('like') }}\">
-                        <input type=\"text\" name=\"id\" value=\"{{ movie.id }}\" style=\"display: none\">
-                        <a class=\"my-list-button\"
-                           onclick=\"document.getElementById('like').submit()\">Dodaj do mojej listy</a>
-                    </form>
-                {% endif %}
-            </div>
         </div>
     </div>
-    </body>
+{#    </body>#}
 {% endblock %}
 {% block stylesheets %}
     {{ parent() }}
@@ -330,7 +311,11 @@ class __TwigTemplate_4c3429a81403940e7c5f59dc027eec62a8d487e507414cb9fc663d20c82
             font-family: 'Netflix Sans Regular', \"Helvetica Neue\", \"Helvetica\", Arial, sans-serif;
             font-weight: normal;
         }
-
+        iframe {
+            position: relative;
+            /*bottom: 50px;*/
+            /*z-index: -1;*/
+        }
         {#======================
                  Buttons
         ======================#}
@@ -364,10 +349,20 @@ class __TwigTemplate_4c3429a81403940e7c5f59dc027eec62a8d487e507414cb9fc663d20c82
         }
 
         .logo {
+            position: absolute;
             height: 31px;
-            margin-right: 15px;
+            right: 1760px;
+            top: 30px;
+            z-index: 2;
+        }
+        .movie_container {
+             margin-left: 0px;
         }
     </style>
+
+{% endblock %}
+{% block javascript %}
+
 
 {% endblock %}
 ", "movies/show.html.twig", "/var/www/symfony_docker/templates/movies/show.html.twig");
