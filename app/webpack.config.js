e7dbdf84f9764b12c2b3aa528dedaee63e778de0
Norivo7 <szymik.kamil97@gmail.com>
2023-07-23 12:46:42 +0200

    up outdated packages, fix encore, 2G patch for dockerfile

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -44,10 +44,10 @@ Encore
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
-
-    .configureBabel((config) => {
-        config.plugins.push('@babel/plugin-proposal-class-properties');
-    })
+    //
+    // .configureBabel((config) => {
+    //     config.plugins.push('@babel/plugin-proposal-class-properties');
+    // })
 
     // enables @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
