4b6dfec1899badabcdb6b130d7f610da82fa5b8d
Norivo7 <szymik.kamil97@gmail.com>
2023-07-23 13:30:32 +0200

    further cleaning, fixing twig dumper

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
